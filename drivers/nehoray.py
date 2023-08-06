b22cfae145f3ef568deee4f42948d0e309b804a3
nehorayp <nehorayp11@gmail.com>
2023-08-06 15:36:49 +0300

    added drivers and nehoray driver

---
+++ drivers/nehoray.py
@@ -0,0 +1,10 @@
+"""
+This driver named Nehoray. This is the best driver in whole ROSE universe!
+"""
+from rose.common import obstacles, actions  # NOQA
+
+driver_name = "No Driver"
+
+
+def drive(world):
+    return actions.NONE
